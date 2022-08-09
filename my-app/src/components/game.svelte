9813f909f9775a57001462f6f0561acf28e4077b
mkrenn <mike.krenn@olai.com>
2022-08-09 08:41:44 +0200

    three.js test

--- my-app/src/components/game.svelte
+++ my-app/src/components/game.svelte
@@ -4,6 +4,8 @@
 	import { onMount } from 'svelte';
 	//@ts-ignore
 	import * as THREE from 'three';
+	//@ts-ignore
+	import { Clock } from 'three';
 
 	export let user: { email: string; username: string; authenticated: boolean };
 
@@ -12,31 +14,45 @@
 			document.body.style.overflow = 'hidden';
 			const frustumSize = 5;
 			let aspect = window.innerWidth / window.innerHeight;
-
+			const clock = new THREE.Clock();
 			const scene = new THREE.Scene();
+			let delta = 0;
+			let time = 0;
 
 			const camera = new THREE.OrthographicCamera(
 				frustumSize * aspect / -2,
 				frustumSize * aspect / 2,
 				frustumSize / 2,
 				frustumSize / -2,
-				0.1,
-				100
+				-10,
+				1000
 			);
-			//rotate camera 90 degrees
-			camera.rotation.x = 90;
-			camera.position.z = 1;
+
 			//set background transparent
-			const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+			const renderer = new THREE.WebGLRenderer({ alpha: false, antialias: true });
 			renderer.setSize(window.innerWidth, window.innerHeight);
 			document.body.appendChild(renderer.domElement);
-
+			//wireframe mode
             const geometry = new THREE.BoxGeometry(1, 1, 1);
-            const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+            const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
             const cube = new THREE.Mesh(geometry, material);
             scene.add(cube);
-
+			
 			function render() {
+				delta = clock.getDelta();
+				
+				cube.rotation.x += 0.01;
+				cube.rotation.y += 0.01;
+				
+
+				//custom loop 
+				let timeToReach = 0.5
+				time += delta;
+				if (time > timeToReach) {
+					time = 0;
+					cube.material.color.setHex(Math.random() * 0xffffff);				
+				}
+
 				requestAnimationFrame(render);
 				renderer.render(scene, camera);
 			}
@@ -60,6 +76,8 @@
 
 		if (user.authenticated) {
 			initThreeScene();
+			console.log('init three');
+			
 		}
 	});
 </script>
