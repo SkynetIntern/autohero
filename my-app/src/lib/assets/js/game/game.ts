59b1a6c51463c37c82666165d0de2379b7adb885
mkrenn <mike.krenn@olai.com>
2022-08-29 14:12:58 +0200

    game class

---
+++ my-app/src/lib/assets/js/game/game.ts
@@ -0,0 +1,77 @@
+//@ts-ignore
+import { bind, init } from 'svelte/internal';
+import * as THREE from 'three';
+//@ts-ignore
+import Stats from 'three/examples/jsm/libs/stats.module';
+
+export default class Game {
+    clock: THREE.Clock = new THREE.Clock();
+    scene: THREE.Scene = new THREE.Scene();
+    delta: number = 0;
+    time: number = 0;
+    stats: Stats = null;
+    frustumSize: number = 5;
+    aspect: number = window.innerWidth / window.innerHeight;
+    camera: THREE.camera = null;
+    renderer: THREE.renderer = new THREE.WebGLRenderer({ alpha: false, antialias: true });
+
+    constructor() {
+        this.stats = new Stats();
+        this.clock = new THREE.Clock();
+        this.clock.start();
+
+        this.camera = new THREE.OrthographicCamera(
+            (this.frustumSize * this.aspect) / -2,
+            (this.frustumSize * this.aspect) / 2,
+            this.frustumSize / 2,
+            this.frustumSize / -2,
+            -10,
+            1000
+        );
+
+        this.init();
+        window.addEventListener('resize', this.onWindowResize.bind(this), false);
+    }
+
+    init() {
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        const renderParent = document.getElementById('three-canvas');
+        if (renderParent == null) return;
+        renderParent.appendChild(this.renderer.domElement);
+        renderParent.appendChild(this.stats.dom);
+
+
+        this.render()
+    }
+
+    render() {
+        requestAnimationFrame(this.render.bind(this));
+        
+        this.delta = this.clock.getDelta();
+        this.stats.update();
+
+        //custom loop
+        let timeToReach = 1;
+        this.time += this.delta;
+        if (this.time > timeToReach) {
+            this.time = 0;
+            //custom loop content here
+
+        }
+
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    //resize function
+    onWindowResize() {
+        this.aspect = window.innerWidth / window.innerHeight;
+
+        this.camera.left = (this.frustumSize * this.aspect) / -2;
+        this.camera.right = (this.frustumSize * this.aspect) / 2;
+        this.camera.top = this.frustumSize / 2;
+        this.camera.bottom = -this.frustumSize / 2;
+
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+}
--- my-app/src/lib/components/game.svelte
+++ my-app/src/lib/components/game.svelte
@@ -2,11 +2,7 @@
 	//@ts-ignore
 	import { io } from '$lib/assets/js/socket/socketinit.js';
 	import { onMount } from 'svelte';
-	//@ts-ignore
-	import * as THREE from 'three';
-	//@ts-ignore
-	import { Clock } from 'three';
-	import Stats from 'three/examples/jsm/libs/stats.module';
+import Game from '../assets/js/game/game';
 
 	export let user: { email: string; username: string; authenticated: boolean };
 	let characters: { name: string; level: number; expCurrent: number; expToNextLevel: number }[] =
@@ -63,63 +59,7 @@
 	onMount(async () => {
 		function initThreeScene() {
 			document.body.style.overflow = 'hidden';
-			const frustumSize = 5;
-			let aspect = window.innerWidth / window.innerHeight;
-			const clock = new THREE.Clock();
-			const scene = new THREE.Scene();
-			let delta = 0;
-			let time = 0;
-
-			const camera = new THREE.OrthographicCamera(
-				(frustumSize * aspect) / -2,
-				(frustumSize * aspect) / 2,
-				frustumSize / 2,
-				frustumSize / -2,
-				-10,
-				1000
-			);
-
-			//set background transparent
-			const renderer = new THREE.WebGLRenderer({ alpha: false, antialias: true });
-			renderer.setSize(window.innerWidth, window.innerHeight);
-			const renderParent = document.getElementById('three-canvas');
-			if (renderParent == null) return;
-
-			renderParent.appendChild(renderer.domElement);
-			//@ts-ignore
-			let stats = new Stats();
-			renderParent.appendChild(stats.dom);
-
-			function render() {
-				delta = clock.getDelta();
-				stats.update();
-
-				//custom loop
-				let timeToReach = 1;
-				time += delta;
-				if (time > timeToReach) {
-					time = 0;
-				}
-
-				requestAnimationFrame(render);
-				renderer.render(scene, camera);
-			}
-
-			//resize function
-			function onWindowResize() {
-				aspect = window.innerWidth / window.innerHeight;
-
-				camera.left = (frustumSize * aspect) / -2;
-				camera.right = (frustumSize * aspect) / 2;
-				camera.top = frustumSize / 2;
-				camera.bottom = -frustumSize / 2;
-
-				camera.updateProjectionMatrix();
-				renderer.setSize(window.innerWidth, window.innerHeight);
-			}
-			window.addEventListener('resize', onWindowResize, false);
-
-			render();
+			let game = new Game();
 		}
 
 		if (user.authenticated) {
